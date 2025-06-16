afd93e674a07efbd0ffc1c63842fb0cb9b2bb495
Samarthh28 <samarthyadav2807@gmail.com>
2025-06-16 10:28:05 +0530

    admin added

---
+++ admin/sidebar.php
@@ -0,0 +1,135 @@
+<!-- Sidebar -->
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+            <a href="index.html" class="logo">
+                <img src="assets/img/kaiadmin/logo_light.svg" alt="navbar brand" class="navbar-brand" height="20" />
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                <li class="nav-item active">
+                    <a href="index.php" class="collapsed">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="Category-list.php" class="collapsed">
+                        <i class="fas fa-chevron-circle-right"></i>
+                        <p>Categoryes</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#base">
+                        <i class="fas fa-layer-group"></i>
+                        <p>Base</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="base">
+                        <ul class="nav nav-collapse">
+                            <a href="components/notifications.html">
+                                <span class="sub-item">Notifications</span>
+                            </a>
+                </li>
+                <li>
+                    <a href="components/sweetalert.html">
+                        <span class="sub-item">Sweet Alert</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="components/font-awesome-icons.html">
+                        <span class="sub-item">Font Awesome Icons</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="components/simple-line-icons.html">
+                        <span class="sub-item">Simple Line Icons</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        </li>
+        <li class="nav-item">
+            <a data-bs-toggle="collapse" href="#tables">
+                <i class="fas fa-table"></i>
+                <p>Tables</p>
+                <span class="caret"></span>
+            </a>
+            <div class="collapse" id="tables">
+                <ul class="nav nav-collapse">
+                    <li>
+                        <a href="tables/tables.html">
+                            <span class="sub-item">Basic Table</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="tables/datatables.html">
+                            <span class="sub-item">Datatables</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a data-bs-toggle="collapse" href="#charts">
+                <i class="far fa-chart-bar"></i>
+                <p>Charts</p>
+                <span class="caret"></span>
+            </a>
+            <div class="collapse" id="charts">
+                <ul class="nav nav-collapse">
+                    <li>
+                        <a href="charts/charts.html">
+                            <span class="sub-item">Chart Js</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="charts/sparkline.html">
+                            <span class="sub-item">Sparkline</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a href="widgets.html">
+                <i class="fas fa-desktop"></i>
+                <p>Widgets</p>
+                <span class="badge badge-success">4</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="../../documentation/index.html">
+                <i class="fas fa-file"></i>
+                <p>Documentation</p>
+                <span class="badge badge-secondary">1</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a data-bs-toggle="collapse" href="#submenu">
+                <i class="fas fa-bars"></i>
+                <p>Menu Levels</p>
+            </a>
+        </li>
+        </ul>
+    </div>
+</div>
+</div>
+<!-- End Sidebar -->
